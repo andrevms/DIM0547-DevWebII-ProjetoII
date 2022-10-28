c15e9add0098e4a5e4de5836b5da2923d2ffdff9
André Soares <37581899+andrevms@users.noreply.github.com>
2022-10-28 08:50:23 -0300

    init project rest api with security

---
+++ spring-rest-security-api/src/main/java/br/com/projetorestsecurity/springrestsecurityapi/SpringRestSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetorestsecurity.springrestsecurityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSecurityApiApplication.class, args);
+	}
+
+}
---
+++ spring-rest-security-api/src/test/java/br/com/projetorestsecurity/springrestsecurityapi/SpringRestSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetorestsecurity.springrestsecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
